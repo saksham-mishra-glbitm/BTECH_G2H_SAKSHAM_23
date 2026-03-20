3a822b00ee76b7d6e61f2f029e06d0083872db61
saksham-mishra-glbitm <kenzydope37@gmail.com>
2026-03-20 11:45:30 +0530

    seventeen.c
    Sum of digits of a number.

--- seventeen.c
+++ seventeen.c
@@ -6,8 +6,15 @@ int main()
     int num=0;
     printf("Enter a number:\n");
     scanf("%d",&num);
+    int t=num,sum=0;
+    
+    while(t!=0)
+    {
+        sum+=t%10;
+        t/=10;
+    }
 
-    printf("\nSum of digits of %d = %d.",num ,(int)(log10(num))+1);
+    printf("\nSum of digits of %d = %d.",num ,sum);
     
     return 0;
 }
